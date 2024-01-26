b66d03a4c2b6f3c8ae0a0ef0088e4284f5d6f01b
Crowdbotics <team@crowdbotics.com>
2024-01-26 22:09:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "retailsupplychain-45622",
+  url: "https://retailsupplychain-45622.botics.co",
+  api: "https://retailsupplychain-45622.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
